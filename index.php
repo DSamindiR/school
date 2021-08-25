304a92e3095aa92b90a0ae63dc2b3662a326e8cb
DSamindiR <88878040+DSamindiR@users.noreply.github.com>
2021-08-25 12:04:56 +0530

    login

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php include_once('header.php');  ?>
+
+<?php 
+// We need to use sessions, so you should always start sessions using the below code. 
+
+    session_start(); 
+
+    // If the user is not logged in redirect to the login page... 
+
+    if (!isset($_SESSION['loggedin'])) { 
+
+        header('Location: login.html'); 
+        exit; 
+    } 
+?>
+
 <?php include_once('footer.php');  ?>
